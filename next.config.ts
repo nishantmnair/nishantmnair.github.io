31cff6973f042c3b95ec1ea5b923cdb035292530
Nishant Nair <nishantnair@Nishants-MacBook-Pro.local>
2026-01-04 18:18:14 -0500

    revert to github pages deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: isProd ? '/portfolio' : '',
+  images: {
+    unoptimized: true,
+  },
   reactCompiler: true,
 };
 
